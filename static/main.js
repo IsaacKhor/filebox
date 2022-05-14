6e422d48f566782f7d647ca91f3444c010a442fb
Isaac Khor <s.github@isaackhor.com>
2022-05-14 21:56:01 +0800

    Add async upload and implement deleting files

--- static/main.js
+++ static/main.js
@@ -2,6 +2,8 @@ window.onload = () => main()
 
 let selectedFiles = new Set()
 let tableRows = undefined
+let opsInProgress = 0
+let uploadXhr = undefined
 
 function main() {
     // Table selections
@@ -18,6 +20,8 @@ function main() {
     document.getElementById('btn-download').addEventListener('click', downloadFiles)
     document.getElementById('btn-delete').addEventListener('click', deleteFiles)
     document.getElementById('btn-create-view').addEventListener('click', createView)
+    document.getElementById('btn-upload-file').addEventListener('click', uploadFiles)
+    document.getElementById('btn-cancel-upload').addEventListener('click', cancelUpload)
 }
 
 function onTableRowClick(row, event) {
@@ -40,12 +44,62 @@ function getCheckboxFromRow(row) {
     return row.querySelector('input[type=checkbox]')
 }
 
+function cancelUpload(ev) {
+    ev.preventDefault()
+    uploadXhr.abort()
+    uploadXhr = null
+}
+
+function uploadFiles(ev) {
+    ev.preventDefault()
+
+    // Cancel any previous uploads
+    if (uploadXhr) uploadXhr.abort()
+
+    let progressBar = document.querySelector('progress#file-upload-progbar')
+    let progText = document.querySelector('span#file-upload-progtext')
+    let xhr = newXhrForPath("POST", "/files")
+    let fd = new FormData(document.querySelector('form#file-upload-form'))
+
+    console.info(`Uploading ${fd.getAll('files').length} files.`)
+
+    xhr.upload.addEventListener('progress', ev => {
+        progressBar.value = ev.loaded / ev.total * 100
+        progText.textContent = `Progress: ${ev.loaded}/${ev.total}`
+    })
+    xhr.upload.addEventListener('load', ev => window.location.reload())
+    xhr.upload.addEventListener('error', ev => {
+        console.error(ev)
+        alert("An error occurred during file upload. Check logs for details")
+    })
+    uploadXhr = xhr
+    xhr.send(fd)
+}
+
 function downloadFiles() {
     alert("Not implemented")
 }
 
 function deleteFiles() {
-    alert("Not implemented")
+    console.log(`Deleting ${selectedFiles}`)
+    for(let f of selectedFiles) {
+        let xhr = newXhrForPath("DELETE", "files/" + f)
+        opsInProgress++
+        xhr.onload = event => {
+            opsInProgress--
+            if(opsInProgress === 0) {
+                window.location.reload()
+            }
+        }
+        console.log(`XHR DELETE request for ${f}`)
+        xhr.send()
+    }
+}
+
+function newXhrForPath(method, path) {
+    let req = new XMLHttpRequest()
+    req.open(method, path)
+    return req
 }
 
 function createView() {
